1b6e7222e1ecda01ff6d2609b79125b54d7c9e43
Haifa_hp <Haifa_hp@DESKTOP-K689OJ3>
2022-09-08 10:42:13 +0430

    End of section2

---
+++ API/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        // when we start our application ,first check if we have database or not,if not we efectevly create or database
+        // we do it in program class
+        public static async Task Main(string[] args)
+        {
+          var host=  CreateHostBuilder(args).Build();
+          using var scop=host.Services.CreateScope();
+          var Services =scop.ServiceProvider;
+          try{
+            var context=Services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+            await Seed.SeedData(context);
+          }
+          catch(Exception ex ){
+            var logger=Services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex,"An error occured during migration");
+          }
+            //we need run application
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
