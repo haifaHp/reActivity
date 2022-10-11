09b9bf0c35d62ad7618bd26ef0d9d02f887fed74
Haifa_hp <Haifa_hp@DESKTOP-K689OJ3>
2022-10-11 09:25:58 +0330

    axios l2

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,11 @@
 import React, {  useEffect, useState } from 'react';
-import axios from 'axios';
+
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import{v4 as uuid} from 'uuid';
+import agent from '../api/agent';
 
 
 function App() {
@@ -14,10 +15,9 @@ function App() {
 const[editMode,setEditMode]=useState(false);
 
   useEffect(() => {
-   axios.get<Activity[]>('http://localhost:5000/api/activities').then(
+   agent.Activities.list().then(
     response=>{
-      console.log(response.data);
-      setActivities(response.data)
+           setActivities(response)
     })
   }, [])
 
